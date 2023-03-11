8ceb640b0d09ef6873e89a358f53c68315cd49f7
Colleen Barbato <colleen@barbato.io>
2023-03-11 11:07:29 -0500

    cat face

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
 let y = 0
 let x = 0
+music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.InBackground)
 radio.setGroup(1)
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . # . #
+        . # . # .
+        `)
+})
 basic.forever(function () {
     x = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.X), 1023, 0, -100, 100)
     y = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.Y), 1023, 0, -100, 100)
