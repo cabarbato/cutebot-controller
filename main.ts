fe41e3f1b7fe05b5c91d52b28d67eed32c793ada
Colleen Barbato <colleen@barbato.io>
2023-03-11 14:10:50 -0500

    initialize controller

--- main.ts
+++ main.ts
@@ -1,23 +1,25 @@
-joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
-    radio.sendValue("emote", 2)
-    basic.showIcon(IconNames.Angry)
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.up, function () {
+    radio.sendValue("emote", 0)
+    basic.showIcon(IconNames.Heart)
 })
-joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
-    radio.sendValue("emote", 3)
-    basic.showIcon(IconNames.Asleep)
-})
-joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.up, function () {
     radio.sendValue("emote", 1)
     basic.showIcon(IconNames.Sad)
 })
-joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
-    radio.sendValue("emote", 0)
-    basic.showIcon(IconNames.Heart)
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.up, function () {
+    radio.sendValue("emote", 3)
+    basic.showIcon(IconNames.Asleep)
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.up, function () {
+    radio.sendValue("emote", 2)
+    basic.showIcon(IconNames.Angry)
 })
 let y = 0
 let x = 0
 music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.InBackground)
 radio.setGroup(1)
+joystickbit.initJoystickBit()
+joystickbit.Vibration_Motor(100)
 basic.forever(function () {
     basic.showLeds(`
         . . . . .
