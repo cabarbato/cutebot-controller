bde4fcd8ef91c990f4d6ad99b6d9dbee4bb4d5dc
Colleen Barbato <colleen@barbato.io>
2023-03-11 19:02:11 -0500

    update emotes

--- main.ts
+++ main.ts
@@ -1,26 +1,18 @@
-joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.up, function () {
-    radio.sendValue("emote", 0)
-    basic.showIcon(IconNames.Heart)
-})
-joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.up, function () {
-    radio.sendValue("emote", 1)
-    basic.showIcon(IconNames.Sad)
-})
-joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.up, function () {
-    radio.sendValue("emote", 3)
-    basic.showIcon(IconNames.Asleep)
-})
-joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.up, function () {
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
     radio.sendValue("emote", 2)
     basic.showIcon(IconNames.Angry)
 })
-let y = 0
-let x = 0
-music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.InBackground)
-radio.setGroup(1)
-joystickbit.initJoystickBit()
-joystickbit.Vibration_Motor(100)
-basic.forever(function () {
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
+    radio.sendValue("emote", 3)
+    basic.showIcon(IconNames.Asleep)
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
+    radio.sendValue("emote", 1)
+    basic.showIcon(IconNames.Sad)
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
+    radio.sendValue("emote", 0)
+    basic.showIcon(IconNames.Heart)
     basic.showLeds(`
         . . . . .
         . # . # .
@@ -28,6 +20,22 @@ basic.forever(function () {
         # . # . #
         . # . # .
         `)
+})
+let y = 0
+let x = 0
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . # . #
+    . # . # .
+    `)
+music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.InBackground)
+radio.setGroup(1)
+joystickbit.initJoystickBit()
+joystickbit.Vibration_Motor(100)
+basic.forever(function () {
+	
 })
 basic.forever(function () {
     x = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.X), 1023, 0, -100, 100)
